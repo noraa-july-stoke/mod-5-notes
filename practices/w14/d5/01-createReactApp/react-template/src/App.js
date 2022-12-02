0e6b6b980d92eda1f7fb9891a1fec1d2c05b9bdb
Noraa Stoke <noraa.july.stoke@gmail.com>
2022-12-02 15:09:07 -0500

    ok

---
+++ practices/w14/d5/01-createReactApp/react-template/src/App.js
@@ -0,0 +1,36 @@
+
+
+function App() {
+  return(
+    <h1>Hello from the app!</h1>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
